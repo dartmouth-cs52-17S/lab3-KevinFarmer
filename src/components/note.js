3beeff37d497d79159271cbb5ae1ff1dfe93df32
Kevin Farmer <kevin.farmer96@gmail.com>
2017-04-18 21:30:07 -0400

    Attempting to update position

--- src/components/note.js
+++ src/components/note.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import Draggable from 'react-draggable';
+import Textarea from 'react-textarea-autosize';
 
+/* eslint react/self-closing-comp: 0 */
 const Note = (props) => {
+  const onStartDrag = () => {
+    console.log('onStartDrag');
+    const x = 0;
+    const y = 1;
+    return props.updatePos(x, y);
+  };
+
+  const onDrag = () => {
+    console.log('onDrag');
+    const x = 0;
+    const y = 1;
+    return props.updatePos(x, y);
+  };
+
+  const onStopDrag = () => {
+    console.log('onStopDrag');
+    const x = 0;
+    const y = 1;
+    return props.updatePos(x, y);
+  };
+
   return (
     <Draggable
       handle=".note-mover"
       grid={[25, 25]}
       defaultPosition={{ x: 20, y: 20 }}
-      position={{ x: 20, y: 20 }}
-      /* onStart={this.onStartDrag}
-      onDrag={this.onDrag}
-      onStop={this.onStopDrag} */
+      position={{ x: props.note.x, y: props.note.y }}
+      onStart={onStartDrag}
+      onDrag={onDrag}
+      onStop={onStopDrag}
     >
-      <div>
-        <p>{props.note.title}</p>
+      <div className="note">
+        <div className="heading">
+          <p>{props.note.title}</p>
+          <div>
+            <i onClick={props.onDelete} className="fa fa-trash-o" aria-hidden="true"></i>
+            <i className="fa fa-pencil" aria-hidden="true"></i>
+            <i className="fa fa-arrows" aria-hidden="true"></i>
+          </div>
+        </div>
+        <Textarea value={props.note.text}></Textarea>
       </div>
     </Draggable>
   );
--- src/index.js
+++ src/index.js
@@ -25,25 +25,41 @@ class App extends Component {
       notes: this.state.notes.set(count, {
         title: newtitle,
         text: 'I is a note',
-        x: 400,
-        y: 12,
+        x: 20,
+        y: 20,
         zIndex: 26,
       }),
     });
     count += 1;
   }
 
+  deleteNote(id) {
+    this.setState({
+      notes: this.state.notes.delete(id),
+    });
+  }
+
+
+  updatePos(id, newX, newY) {
+    const newNote = Object.assign({}, this.state.note.id, {
+      x: newX,
+      y: newY,
+    });
+    this.setState({
+      notes: this.state.notes.update(id, newNote),
+    });
+  }
+
+
   render() {
     let notesRend;
     if (this.state.notes.size > 0) {
       notesRend = this.state.notes.entrySeq().map(([id, note]) => {
-        return <Note id={id} note={note} />;  // for instance maybe
+        return <Note key={id} note={note} onDelete={() => this.deleteNote(id)} updatePos={(newX, newY) => this.updatePos(id, newX, newY)} />;  // for instance maybe
       });
     } else {
       notesRend = '';
     }
-
-    console.log(this.state.notes);
 
     return (
       <div>
